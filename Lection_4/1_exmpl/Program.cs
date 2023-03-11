8fb9622592f4c3d74be5a8ce344c2f7893cd3ca4
Kalinin <9776020@mail.ru>
2023-03-11 21:42:40 +0300

    Lection 1 example

---
+++ Lection_4/1_exmpl/Program.cs
@@ -0,0 +1,10 @@
+﻿string[,] table = new string[2,5];
+table[1,2] = "word";
+
+for (int rows = 0; rows < 2; rows++)
+{
+  for (int col = 0; col < 5; col++)
+  {
+    Console.WriteLine($"-{table[rows, col]}-");
+  }
+}
