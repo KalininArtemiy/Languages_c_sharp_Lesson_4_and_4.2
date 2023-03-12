811ebfe1476334f46ed599dc015fddb48a87316f
Kalinin <9776020@mail.ru>
2023-03-12 22:25:38 +0300

    Seminar 1_3

---
+++ Seminar/1_3/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int[] Mass(int size, int from, int to)
+{
+  int[] array = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = new Random().Next(from, to);
+  }
+  return array;
+}
+void Print(int[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+int Count(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] >= 10 && array[i] < 100)
+    {
+      count = count + 1;
+    }
+  }
+  return count;
+}
+
+
+
+int[] new_arr = Mass(123, 10, 1000);
+Print(new_arr);
+Console.WriteLine(Count(new_arr));
