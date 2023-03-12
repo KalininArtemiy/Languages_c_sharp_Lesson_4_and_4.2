2818bc8882ff93cf5ec17d15abf78dc6337d5402
Kalinin <9776020@mail.ru>
2023-03-12 21:59:17 +0300

    Seminar 1_2

---
+++ Seminar/1_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+int[] Mass(int size, int from, int to)
+{
+  int[] arr = new int[size];
+  for(int i = 0; i < size; i++)
+  {
+    arr[i] = new Random().Next(from, to);
+  }
+  return arr;
+}
+void PrintMass(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine();
+}
+
+
+void findN(int[] array, int n)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] == n)
+    {
+      Console.WriteLine( "Yes");
+      return;
+    };
+  }
+  Console.WriteLine("No");
+}
+int[] new_arr = Mass(10, -10, 10);
+PrintMass(new_arr);
+int num = 4;
+Console.WriteLine(num);
+findN(new_arr, num);
