b096989f0a61ffc28a48e1c4110fa79469b3ed07
Kalinin <9776020@mail.ru>
2023-03-13 00:42:37 +0300

    HW_3

--- Homework/1_3/Program.cs
+++ Homework/1_3/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿double[] Mass(int size, double from, double to)
+{
+  double[] array = new double[size];
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = Math.Round(new Random().NextDouble() * (to - from) + from, 4);
+  }
+  return array;
+}
+void Print(double[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+
+double Count(double[] array)
+{
+  double max = array[0];
+  double min = array[0];
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > max)
+    {
+      max = array[i];
+    }
+    else if (array[i] < min)
+    {
+      min = array[i];
+    }
+  }
+  double result = max - min;
+  return result;
+}
+
+
+
+double[] new_array = Mass(int.Parse(Console.ReadLine()), 
+double.Parse(Console.ReadLine()),
+double.Parse(Console.ReadLine()));
+Print(new_array);
+Console.WriteLine();
+Console.WriteLine(Count(new_array));
