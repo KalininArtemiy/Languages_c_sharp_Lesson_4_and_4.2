fb11d6f6941be9e20f2f1d233d03ba4e4581aefa
Kalinin <9776020@mail.ru>
2023-03-12 21:34:14 +0300

    Seminar 1_1

---
+++ Seminar/1_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// положительные элементы замените на соответствующие отрицательные, и наоборот.
+int[] Mass(int size, int from, int to)
+{
+  int[] arr = new int[size];
+  for(int i = 0; i < size; i++)
+  {
+    arr[i] = new Random().Next(from, to);
+  }
+  return arr;
+}
+void PrintMas(int[] arr)
+{
+  int size = arr.Length;
+  for (int i = 0; i < size; i ++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine();
+}
+void Revers(int[] arr)
+{
+  int size = arr.Length;
+  for(int i = 0; i < size; i++)
+  {
+    arr[i] = -arr[i];
+  }
+}
+int [] new_arr = Mass(int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()),
+int.Parse(Console.ReadLine()));
+PrintMas(new_arr);
+Revers(new_arr);
+PrintMas(new_arr);
