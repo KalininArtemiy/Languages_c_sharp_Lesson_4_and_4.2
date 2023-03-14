7c90f03f6509830be491695adf7d2320fc475c61
Kalinin <9776020@mail.ru>
2023-03-15 00:16:47 +0300

    S_1_1

---
+++ Seminar_2/1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void Is_trungle(int a, int b, int c)
+{
+  if(a < b + c && b < a + c && c < a + b) 
+  Console.WriteLine("Yes");
+  else 
+  Console.WriteLine("No");
+}
+Console.WriteLine("Enter a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter c");
+int c = int.Parse(Console.ReadLine());
+Is_trungle(a,b,c);
