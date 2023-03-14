c065271dd34bd2c71be196f807233c210186b625
Kalinin <9776020@mail.ru>
2023-03-15 01:25:13 +0300

    S_1_3

---
+++ Seminar_2/1_3/Program.cs
@@ -0,0 +1,12 @@
+﻿void Fibonachi(int num)
+{
+  int a = 0;
+  int b = 1;
+  for( int i = 0; i < num; i++)
+  {
+    Console.Write(b + " ");
+    (b,a) = (a,b+a);
+  }
+}
+int num = 10;
+Fibonachi(num);
