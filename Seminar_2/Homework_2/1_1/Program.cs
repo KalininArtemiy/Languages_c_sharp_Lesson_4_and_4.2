65cf6c43d12c3ef785178f4ee176e58610691f33
Kalinin <9776020@mail.ru>
2023-03-18 23:43:50 +0300

    HW 1

--- Seminar_2/Homework_2/1_1/Program.cs
+++ Seminar_2/Homework_2/1_1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Value ()
+{
+  Console.Write("How many symbols are you going write?");
+  int num = int.Parse(Console.ReadLine());
+  return num;
+}
+
+
+int[] enter_mass(int num)
+{
+  int[] arr = new int[num];
+  for (int i=0; i < arr.Length; i++)
+  {
+    arr[i] = int.Parse(Console.ReadLine());
+  }
+  return arr;
+}
+
+void up_zero(int[] arr)
+{
+  int count = 0;
+  for (int i=0; i < arr.Length; i++)
+  {
+    if( arr[i] > 0)
+    {
+      count = count +1;
+    } 
+  }
+  Console.WriteLine(count);
+}
+
+
+void Print(int[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+
+int symbols = Value();
+int[] new_arr = enter_mass(symbols);
+Print(new_arr);
+Console.WriteLine();
+up_zero(new_arr);
