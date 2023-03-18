1dadf75df3c1f4f4e0dc9ca05c12a6ce9cfd4e88
Kalinin <9776020@mail.ru>
2023-03-19 00:08:45 +0300

    HW 2

---
+++ Seminar_2/Homework_2/1_2/Program.cs
@@ -0,0 +1,28 @@
+﻿void CrossLine(double b1, double k1, double b2, double k2)
+{
+  if (b1 == b2 && k1 == k2)
+  {
+    Console.WriteLine("They are similiar");
+  }
+  else if (k1 == k2)
+  {
+    Console.WriteLine("They are parallel");
+  }
+  else 
+  {
+    double cross_x = (b2 - b1)/(k1 - k2);
+    double cross_y = (k1 * (b2 - b1))/(k2 - k1) + b1;
+    Console.WriteLine($"cross_x = {cross_x}");
+    Console.Write($"cross_y = {cross_y}");
+  }
+}
+
+Console.WriteLine("enter b1");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter k1");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter b2");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter k2");
+int k2 = int.Parse(Console.ReadLine());
+CrossLine (b1, k1, b2, k2);
